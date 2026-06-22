c19a03793a3ae7cefbb46754bd9da137cb1ecf68
Radiology Bot Developer <mr981243@gmail.com>
2026-06-22 15:58:06 +0430

    update features

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
-from telegram import Update
+ffrom telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from database import conn, cursor   # 👈 این باید بالا باشه
 
 TOKEN = "8723545702:AAFDjnjIj3-ZQ79X0y_5E4YHLtVXdqtA-SI"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO users (user_id, name) VALUES (?, ?)",
+        (user.id, user.first_name)
+    )
+    conn.commit()
+
     await update.message.reply_text("سلام 👋 به ربات انجمن علمی رادیولوژی خوش اومدی")
 
 app = ApplicationBuilder().token(TOKEN).build()
@@ -12,4 +21,3 @@ app.add_handler(CommandHandler("start", start))
 
 print("Bot is running...")
 app.run_polling()
-from database import conn, cursor

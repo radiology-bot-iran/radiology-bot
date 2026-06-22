ea0223159108ecad02ebc17108254ea50d4d8b96
Radiology Bot Developer <mr981243@gmail.com>
2026-06-22 14:08:22 +0430

    first bot

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8723545702:AAFDjnjIj3-ZQ79X0y_5E4YHLtVXdqtA-SI"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 به ربات انجمن علمی رادیولوژی خوش اومدی")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+app.run_polling()
